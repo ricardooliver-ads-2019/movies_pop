58a0d1ecc1583736af4cc1500eba74dcf4a1b012
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-14 13:58:50 -0400

    feat(home/data/models): Adicionando os modelos de contratos da camada de data     - Adicionando o modelo de filme (MovieModel).     - Adicionando o modelo de generos (GenereModel).

---
+++ lib/features/movies/home/data/models/genere_model.dart
@@ -0,0 +1,8 @@
+import 'package:movies_pop/features/movies/home/domain/entities/genere_entipy/genere_enpity.dart';
+
+class GenereModel extends GenereEnpity {
+  GenereModel({
+    required int id,
+    required String name,
+  }) : super(id: id, name: name);
+}
---
+++ lib/features/movies/home/data/models/movie_model.dart
@@ -0,0 +1,33 @@
+import 'package:movies_pop/features/movies/home/domain/entities/movie_entipy/movie_entipy.dart';
+
+class MovieModel extends MovieEntipy {
+  const MovieModel(
+      {required int id,
+      required String title,
+      required String? posterPath,
+      required double stars,
+      required DateTime releaseDate})
+      : super(
+          id: id,
+          title: title,
+          posterPath: posterPath,
+          stars: stars,
+          releaseDate: releaseDate,
+        );
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
+        id: json['id'],
+        title: json['title'],
+        posterPath: json['poster_path'],
+        stars: json['vote_average'],
+        releaseDate: json['release_date'],
+      );
+
+  Map<String, dynamic> toJoson() => {
+        'id': id,
+        'title': title,
+        'poster_path': posterPath,
+        'vote_average': stars,
+        'release_date': releaseDate,
+      };
+}
