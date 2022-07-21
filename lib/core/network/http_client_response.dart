d9daa2c154316efee077e5f6584966c7aed76f5b
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-21 07:13:37 -0400

    refactor(http_client_response): Alterando os atributos do HttpClientResponseError.     - Excluindo atributos desnecessarios pois os mesmos estavam sobresquevendo os atributos ja herdados da class abstrac

--- lib/core/network/http_client_response.dart
+++ lib/core/network/http_client_response.dart
@@ -18,25 +18,15 @@ class HttpClientResponseSuccess extends HttpClientResponse {
 }
 
 class HttpClientResponseError extends HttpClientResponse {
-  String? message;
-  @override
-  int? statusCode;
-  dynamic error;
-  HttpClientResponse? response;
-
   HttpClientResponseError({
     required data,
     required int? statusCode,
     required String? statusMessage,
     HttpClientResponse? response,
-  }) : super(
-          data: data,
-          statusCode: statusCode,
-          statusMessage: statusMessage,
-        );
+  }) : super(data: data, statusCode: statusCode, statusMessage: statusMessage);
 
   @override
   String toString() {
-    return 'HttClientException(message: $message, statusCode: $statusCode, error: $error, response: $response)';
+    return 'HttClientResponseError(statusCode: $statusCode, statusMessage: $statusMessage)';
   }
 }
