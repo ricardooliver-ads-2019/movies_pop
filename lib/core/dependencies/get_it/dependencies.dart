82fe4cf937fd31c4fcde788c5f5c40c8f40c3ee3
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-21 07:55:57 -0400

    feat: Adicionando novas dependencias que serão compartilhada pelas camadas

--- lib/core/dependencies/get_it/dependencies.dart
+++ lib/core/dependencies/get_it/dependencies.dart
@@ -1,9 +1,18 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:movies_pop/core/network/dio/dio_client.dart';
 import 'package:movies_pop/core/network/http_client.dart';
+import 'package:movies_pop/core/network_connection/i_network_connection.dart';
+import 'package:movies_pop/core/network_connection/network_connection_impl.dart';
 
 GetIt getItDependency = GetIt.instance;
 
 Future<void> getItDependencies() async {
-  getItDependency.registerLazySingleton<HttpClient>(() => DioClient());
+  getItDependency
+      .registerSingleton<DataConnectionChecker>(DataConnectionChecker());
+  getItDependency.registerFactory<INetworkConnection>(() =>
+      NetworkConnectionImpl(
+          connectionChecker: getItDependency<DataConnectionChecker>()));
+  getItDependency.registerLazySingleton<HttpClient>(() =>
+      DioClient(networkConnection: getItDependency<NetworkConnectionImpl>()));
 }
