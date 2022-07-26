52f575b598fda5a76dcfb6fb24d5bf8fc0c489f5
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-26 15:51:10 -0400

    fix: Corrigindo erro de injeção de dependencies na cama de network_connection     - Corrigindo bug na injeção de dependencies causado quando a camada de network_connection_impl era estanciada, pois o package DataConnectionChecker estava sendo passado como uma dependencies requerida no construtor da class, mas agora a mesa esta enjetada como um atributo ta class não sendo mais necessario a injeção da DataConnectionChecker como dependencies requerida em seu construtor e com isso o erro na sua instanciação foi Corrigindo.

--- lib/core/network_connection/network_connection_impl.dart
+++ lib/core/network_connection/network_connection_impl.dart
@@ -2,9 +2,8 @@ import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:movies_pop/core/network_connection/i_network_connection.dart';
 
 class NetworkConnectionImpl implements INetworkConnection {
-  final DataConnectionChecker connectionChecker;
+  final DataConnectionChecker connectionChecker = DataConnectionChecker();
 
-  NetworkConnectionImpl({required this.connectionChecker});
   @override
   Future<bool> get isConnected => connectionChecker.hasConnection;
 }
