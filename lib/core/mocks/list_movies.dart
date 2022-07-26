a274bd118f874096612ea5f724d85507c9e32cfd
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-26 17:00:19 -0400

    feat: Adicionando uma lista de Filmes em Mock     - Criando uma lista de Filmes em mock para criar e testas os componentes de tela e o layout da aplicação.

---
+++ lib/core/mocks/list_movies.dart
@@ -0,0 +1,46 @@
+import '../../features/movies/home/domain/entities/movie_entipy/movie_entipy.dart';
+
+final List<MovieEntipy> listMoviesMock = [
+  MovieEntipy(
+    id: 825808,
+    title: "See for Meeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee Star",
+    posterPath: "qk1ZERG6yhwAJqTobmDgw8jRL2C.jpg",
+    stars: 6.3,
+    releaseDate: DateTime.parse('2022-01-07'),
+  ),
+  MovieEntipy(
+    id: 825808,
+    title: "See for Me",
+    posterPath: "qk1ZERG6yhwAJqTobmDgw8jRL2C.jpg",
+    stars: 6.3,
+    releaseDate: DateTime.parse('2022-01-07'),
+  ),
+  MovieEntipy(
+    id: 825808,
+    title: "See for Me",
+    posterPath: "qk1ZERG6yhwAJqTobmDgw8jRL2C.jpg",
+    stars: 6.3,
+    releaseDate: DateTime.parse('2022-01-07'),
+  ),
+  MovieEntipy(
+    id: 825808,
+    title: "See for Me",
+    posterPath: "qk1ZERG6yhwAJqTobmDgw8jRL2C.jpg",
+    stars: 6.3,
+    releaseDate: DateTime.parse('2022-01-07'),
+  ),
+  MovieEntipy(
+    id: 825808,
+    title: "See for Me",
+    posterPath: "qk1ZERG6yhwAJqTobmDgw8jRL2C.jpg",
+    stars: 6.3,
+    releaseDate: DateTime.parse('2022-01-07'),
+  ),
+  MovieEntipy(
+    id: 825808,
+    title: "See for Me",
+    posterPath: "qk1ZERG6yhwAJqTobmDgw8jRL2C.jpg",
+    stars: 6.3,
+    releaseDate: DateTime.parse('2022-01-07'),
+  )
+];
