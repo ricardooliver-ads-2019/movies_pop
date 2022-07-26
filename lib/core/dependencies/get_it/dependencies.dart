c8ee215c93e6dd88ce750f090a7fd38fbd002cfe
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-26 16:02:27 -0400

    duild: Adicionando as instancias das dependencies com Get_It     - Adicionando as instancias das dependencias da aplicação     (usecase/repositories/datasources/network_connection/httpCleint...)

--- lib/core/dependencies/get_it/dependencies.dart
+++ lib/core/dependencies/get_it/dependencies.dart
@@ -1,18 +1,47 @@
-import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:movies_pop/core/network/dio/dio_client.dart';
 import 'package:movies_pop/core/network/http_client.dart';
 import 'package:movies_pop/core/network_connection/i_network_connection.dart';
 import 'package:movies_pop/core/network_connection/network_connection_impl.dart';
+import 'package:movies_pop/features/main_navigation_page/controller/main_navigation_page_cubit.dart';
+import 'package:movies_pop/features/movies/home/data/datasources/home_datasource_impl.dart';
+import 'package:movies_pop/features/movies/home/data/datasources/i_home_datasource.dart';
+import 'package:movies_pop/features/movies/home/data/repositories/home_repository_impl.dart';
+import 'package:movies_pop/features/movies/home/domain/repositories/home/home_repository.dart';
+import 'package:movies_pop/features/movies/home/domain/usecase/get_movies_playing_in_Brazil_now_usecase.dart';
+import 'package:movies_pop/features/movies/home/domain/usecase/get_movies_popular_usecar.dart';
+import 'package:movies_pop/features/movies/home/presenter/home_controller/home_cubit_controller.dart';
 
 GetIt getItDependency = GetIt.instance;
 
 Future<void> getItDependencies() async {
   getItDependency
-      .registerSingleton<DataConnectionChecker>(DataConnectionChecker());
-  getItDependency.registerFactory<INetworkConnection>(() =>
-      NetworkConnectionImpl(
-          connectionChecker: getItDependency<DataConnectionChecker>()));
-  getItDependency.registerLazySingleton<HttpClient>(() =>
-      DioClient(networkConnection: getItDependency<NetworkConnectionImpl>()));
+      .registerFactory<INetworkConnection>(() => NetworkConnectionImpl());
+
+  getItDependency.registerLazySingleton<HttpClient>(
+      () => DioClient(networkConnection: getItDependency()));
+
+  getItDependency.registerLazySingleton<IHomeDatasource>(
+      () => HomeDatasourceImpl(client: getItDependency<HttpClient>()));
+
+  getItDependency.registerLazySingleton<HomeRepository>(
+      () => HomeRepositoryImpl(datasource: getItDependency<IHomeDatasource>()));
+
+  getItDependency.registerLazySingleton<GetMoviesPlayingInBrazilNowUsecase>(
+      () => GetMoviesPlayingInBrazilNowUsecase(
+          repository: getItDependency<HomeRepository>()));
+
+  getItDependency.registerLazySingleton<GetMoviesPopularUsecar>(() =>
+      GetMoviesPopularUsecar(repository: getItDependency<HomeRepository>()));
+
+//=========================== Cubits ===========================
+  getItDependency.registerFactory<MainNavigationPageCubit>(
+      () => MainNavigationPageCubit());
+
+  getItDependency
+      .registerFactory<HomeCubitController>(() => HomeCubitController(
+            moviesPlayingInBrazilNowUsecase:
+                getItDependency<GetMoviesPlayingInBrazilNowUsecase>(),
+            moviesPopularUsecar: getItDependency<GetMoviesPopularUsecar>(),
+          ));
 }
