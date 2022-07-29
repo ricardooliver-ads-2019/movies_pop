17a3e2cb1b3ed5fda13b7b56f5d0c4bd8c872896
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-29 09:15:10 -0400

    refactor: Alterando o comportamento da tela homa, que agora não terá mais estados     - Refatorando a tela home e componetizando seus elementos, agora a tela home não tem mais responsabidade em getenciar os estados, pois os seus componentes agora possuem estados proprios.

--- lib/features/movies/home/presenter/screens/home_screen.dart
+++ lib/features/movies/home/presenter/screens/home_screen.dart
@@ -1,10 +1,12 @@
-import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:movies_pop/features/movies/home/presenter/home_controller/home_cubit_controller.dart';
+import 'package:movies_pop/core/dependencies/get_it/dependencies.dart';
+import 'package:movies_pop/features/movies/home/presenter/components/popular_movies/movies_group.dart';
+import 'package:movies_pop/features/movies/home/presenter/components/popular_movies/popular_cubit_controller/popular_cubit_controller.dart';
+import 'package:movies_pop/features/movies/home/presenter/components/popular_movies/popular_cubit_controller/popular_state.dart';
 
-import '../widgets/cine_movies/cine_group.dart';
-import '../widgets/popular_movies/movies_group.dart';
+import '../components/cine_movies/cine_group.dart';
+import '../components/cine_movies/cine_movies_cubit_controller/cine_movies_cubit_controller.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -17,51 +19,34 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    final _controller = context.read<HomeCubitController>();
-    _controller.getMoviesPlayingInBrazilNowUsecase(1);
-    _controller.getPageMoviesPopular(1);
   }
 
   @override
   Widget build(BuildContext context) {
     var mediaSize = MediaQuery.of(context).size;
-    return BlocConsumer<HomeCubitController, HomeState>(
-        listenWhen: (previous, current) => previous.status != current.status,
-        listener: (context, state) {
-          if (state.status == HomeStatus.error) {
-            if (state.error != null) {
-              AsukaSnackbar.alert('${state.error!.message}').show();
-            }
-          }
-        },
-        builder: (context, state) {
-          switch (state.status) {
-            case HomeStatus.loading:
-              return const Center(child: CircularProgressIndicator());
-            default:
-              //print(state.pagePopularMovies?.movies);
-              return Scaffold(
-                body: SingleChildScrollView(
-                  child: Column(
-                    //mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      if (state.pageCineMovies != null &&
-                          state.pageCineMovies!.movies.isNotEmpty)
-                        CineGroup(listMovies: state.pageCineMovies?.movies),
-                      // const SizedBox(height: 20),
-                      if (state.pagePopularMovies != null &&
-                          state.pagePopularMovies!.movies.isNotEmpty)
-                        MoviesGroup(
-                            title: 'Filmes Populares',
-                            listMovies: state.pagePopularMovies?.movies),
-                      //const SizedBox(height: 20),
-                    ],
-                  ),
-                ),
-              );
-          }
-        });
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BlocProvider(
+              create: (_) => getItDependency.get<CineMoviesCubitController>(),
+              child: BlocBuilder<CineMoviesCubitController, CineMoviesState>(
+                  buildWhen: (previous, current) => previous != current,
+                  builder: (context, state) {
+                    return const CineGroup();
+                  }),
+            ),
+            BlocProvider(
+              create: (_) => getItDependency.get<PopularCubitController>(),
+              child: BlocBuilder<PopularCubitController, PopularState>(
+                  buildWhen: (previous, current) => previous != current,
+                  builder: (context, state) {
+                    return const MoviesGroup(title: 'Filmes Populares');
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
