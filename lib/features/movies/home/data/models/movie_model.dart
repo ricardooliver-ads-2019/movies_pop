62dd1209621763b5d005bf1cb8be53f196c86c86
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-07-31 13:54:42 -0400

    refactor: Modificando os campos da movie_entipy e movie_model, para aceitar um novo recurso de imagem de background.

--- lib/features/movies/home/data/models/movie_model.dart
+++ lib/features/movies/home/data/models/movie_model.dart
@@ -5,12 +5,14 @@ class MovieModel extends MovieEntipy {
       {required int id,
       required String title,
       required String? posterPath,
+      required String? backdropPath,
       required double stars,
       required DateTime releaseDate})
       : super(
           id: id,
           title: title,
           posterPath: posterPath,
+          backdropPath: backdropPath,
           stars: stars,
           releaseDate: releaseDate,
         );
@@ -20,14 +22,17 @@ class MovieModel extends MovieEntipy {
       id: json['id'],
       title: json['title'],
       posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
       stars: json['vote_average'].toDouble(),
-      releaseDate: DateTime.parse(json['release_date']),
+      releaseDate: DateTime.parse(
+          json['release_date'] == '' ? '2000-02-02' : json['release_date']),
     );
   }
   Map<String, dynamic> toJson() => {
         'id': id,
         'title': title,
         'poster_path': posterPath,
+        'backdrop_path': backdropPath,
         'vote_average': stars,
         'release_date': releaseDate.toString(),
       };
--- lib/features/movies/home/domain/entities/movie_entipy/movie_entipy.dart
+++ lib/features/movies/home/domain/entities/movie_entipy/movie_entipy.dart
@@ -4,10 +4,12 @@ class MovieEntipy extends Equatable {
   final int id;
   final String title;
   final String? posterPath;
+  final String? backdropPath;
   final double stars;
   final DateTime releaseDate;
 
   const MovieEntipy({
+    required this.backdropPath,
     required this.id,
     required this.title,
     required this.posterPath,
