812fba840c6972928b542f84ffef46927222eebc
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-08-06 09:09:30 -0400

    feat: Adicionando a camada de LoginRepositories

---
+++ lib/features/login/domain/repositories/i_login_repository.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_pop/core/erros/failures.dart';
+import 'package:movies_pop/features/login/domain/entities/request_token_entity.dart';
+import 'package:movies_pop/features/login/domain/entities/session_id_entity.dart';
+
+abstract class ILoginRepository {
+  Future<Either<Failure, RequestTokenEntity>> getRequestToken();
+
+  Future<Either<Failure, RequestTokenEntity>> validateTokenWithLogin({
+    required String login,
+    required String password,
+    required String requestToken,
+  });
+
+  Future<Either<Failure, SessionIdEntity>> validateSessionId(
+      {required String requestToken});
+}
