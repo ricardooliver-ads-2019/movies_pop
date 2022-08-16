ff4b3a706daffbf1cddf9c536e5ece26a745dcbc
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-08-16 14:00:05 -0400

    style: Adicionando class com que gerencia todas as strings reutilisaveis na aplicação

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,6 +1,9 @@
 class Constants {
   Constants._();
-  static const base_url = 'https://api.themoviedb.org/3/';
+  static const base_url = 'https://api.themoviedb.org/3';
 
   static const api_key = '899e6212bb39852b379032c9c5e25689';
+  static const sharedPreferencesUsername = 'userName';
+  static const sharedPreferencesAccountId = 'accountId';
+  static const sharedPreferencesSessionId = 'sessionId';
 }
