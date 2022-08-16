aed4b3c419d9a3c11e8a8cdeffedba381923edd9
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-08-16 13:54:02 -0400

    feat: Adicionando rota de login para navegação no app

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_pop/core/dependencies/get_it/dependencies.dart';
+import 'package:movies_pop/features/login/presenter/controller/login_cubit_controller.dart';
+import 'package:movies_pop/features/login/presenter/screen/login_screen.dart';
 import 'package:movies_pop/features/main_navigation_page/controller/main_navigation_page_cubit.dart';
 import 'package:movies_pop/features/main_navigation_page/main_navigation_page.dart';
 import 'package:movies_pop/features/movies/home/presenter/screens/home_screen.dart';
+import 'package:movies_pop/features/splash/splash_screen.dart';
 import 'package:movies_pop/features/watch_movies/watch_movies_screen.dart';
 import 'package:movies_pop/features/watched_movies/watched_movies_screen.dart';
 
 class AppRoutes {
-  static const splash = '/';
+  static const splash = '/splash';
+  static const login = '/login';
   static const mainNavigationPage = '/main_navigation_page';
   static const home = '/home';
   static const watchedMovies = '/watched_movies';
@@ -24,6 +28,16 @@ class AppRoutes {
             child: const MainNavigationPage(),
           ),
         );
+
+      case AppRoutes.splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+
+      case AppRoutes.login:
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider<LoginCubitController>(
+                  create: (_) => getItDependency.get<LoginCubitController>(),
+                  child: const LoginScreen(),
+                ));
 
       case AppRoutes.home:
         return MaterialPageRoute(
