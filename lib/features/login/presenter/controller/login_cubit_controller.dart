906e46383e4c97fcb134892c34549c4bc18f66ef
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-08-16 21:18:00 -0400

    feat: Criando a estrutura da tela de login.     - Adicionando a tela de login e as suas classes responsaveis por conter e gerenciar os estados da tela de login.

---
+++ lib/features/login/presenter/controller/login_cubit_controller.dart
@@ -0,0 +1,84 @@
+import 'package:bloc/bloc.dart';
+import 'package:movies_pop/features/auth/auth.dart';
+import 'package:movies_pop/features/login/domain/usecase/get_details_account.dart';
+import 'package:movies_pop/features/login/domain/usecase/get_request_token_usecase.dart';
+import 'package:movies_pop/features/login/domain/usecase/validate_token_with_login_usecase.dart';
+import 'package:movies_pop/features/login/presenter/controller/login_state.dart';
+
+import '../../domain/usecase/validate_session_id_usecase.dart';
+
+class LoginCubitController extends Cubit<LoginState> {
+  final AuthSession _authSession;
+  final GetRequestTokenUsecase _getRequestTokenusecase;
+  final ValidateTokenWithLoginUsecase _validateTokenWithLoginUsecase;
+  final ValidateSessionIdUsecase _validateSessionIdUsecase;
+  final GetDetailsAccountUsecase _getDetailsAccountUsecase;
+
+  LoginCubitController({
+    required AuthSession authSession,
+    required ValidateTokenWithLoginUsecase validateTokenWithLoginUsecase,
+    required GetRequestTokenUsecase getRequestTokenusecase,
+    required ValidateSessionIdUsecase validateSessionIdUsecase,
+    required GetDetailsAccountUsecase getDetailsAccountUsecase,
+  })  : _authSession = authSession,
+        _getRequestTokenusecase = getRequestTokenusecase,
+        _validateTokenWithLoginUsecase = validateTokenWithLoginUsecase,
+        _validateSessionIdUsecase = validateSessionIdUsecase,
+        _getDetailsAccountUsecase = getDetailsAccountUsecase,
+        super(InitState());
+
+  Future<void> validateTokenWithLoginUsecase({
+    required String login,
+    required String password,
+  }) async {
+    emit(Loading());
+    final result = await _getRequestTokenusecase.call();
+    result.fold((error) {
+      //print(error);
+      emit(Error(error: error));
+    }, (token) async {
+      //print(token);
+      final result = await _validateTokenWithLoginUsecase.call(
+          login: login, password: password, requestToken: token.requestToken);
+      result.fold((error) {
+        //print(error);
+        emit(Error(error: error));
+      }, (token) {
+        //print(token);
+        validateSessionIdUsecase(requestToken: token.requestToken);
+        emit(ValidateTokenSuccess(token: token));
+      });
+    });
+  }
+
+  Future<void> validateSessionIdUsecase({required String requestToken}) async {
+    emit(Loading());
+    final result =
+        await _validateSessionIdUsecase.call(requestToken: requestToken);
+    result.fold((error) {
+      //print(error);
+      emit(Error(error: error));
+    }, (sessionId) {
+      _authSession.saveSessionId(sessionId: sessionId.sessionId);
+      _authSession.sessionId = sessionId.sessionId;
+      emit(SessionIdSuccess(sessionId: sessionId));
+      getDetailsAccount(sessionId: sessionId.sessionId);
+    });
+  }
+
+  Future<void> getDetailsAccount({required String sessionId}) async {
+    emit(Loading());
+    final result = await _getDetailsAccountUsecase.call(sessionId: sessionId);
+    result.fold((error) {
+      //print(error);
+      emit(Error(error: error));
+    }, (detailsAccount) {
+      _authSession.saveDetailsAccount(
+          username: detailsAccount.username, id: detailsAccount.id);
+      _authSession.username = detailsAccount.username;
+      _authSession.id = detailsAccount.id;
+      _authSession.init();
+      emit(LoginSuccess(detailsAccount: detailsAccount));
+    });
+  }
+}
---
+++ lib/features/login/presenter/screen/login_screen.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_pop/core/routes/app_routes.dart';
+import 'package:movies_pop/features/login/presenter/componets/form_login/button_login.dart';
+import 'package:movies_pop/features/login/presenter/componets/form_login/form_login.dart';
+import 'package:movies_pop/features/login/presenter/componets/form_login/widgets/button_create_account.dart';
+import 'package:movies_pop/features/login/presenter/componets/form_login/widgets/button_forgot_password.dart';
+import 'package:movies_pop/features/login/presenter/controller/login_cubit_controller.dart';
+import 'package:movies_pop/features/login/presenter/controller/login_state.dart';
+import 'package:movies_pop/features/login/presenter/widgets/logo.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> animationPosition;
+  // late Animation<double> animationPositionHeight;
+  late Animation<double> animationOpacity;
+
+  late LoginCubitController _controller;
+
+  var animationErro = ValueNotifier(false);
+  late RenderBox? renderBox;
+  String login = '';
+  String password = '';
+
+  @override
+  void didChangeDependencies() {
+    animationPosition = Tween<double>(begin: 0, end: 0).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
+
+    animationOpacity = Tween<double>(begin: 1, end: 0).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeOut));
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = context.read<LoginCubitController>();
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    initAnimated();
+  }
+
+  Future<void> initAnimated() async {
+    _animationController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var mediaSize = MediaQuery.of(context).size;
+    return BlocConsumer<LoginCubitController, LoginState>(
+        listener: (context, state) async {
+      if (state is Error) {
+        animationErro.value = true;
+        final mensagen = state.error.message?.toString() ?? 'Error';
+        final snackBar = SnackBar(
+          content: Text(mensagen),
+          backgroundColor: Colors.red,
+        );
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+      if (state is LoginSuccess) {
+        await _animationController.forward();
+
+        Navigator.of(context)
+            .pushReplacementNamed(AppRoutes.mainNavigationPage);
+
+        await _animationController.reverse();
+      }
+    }, builder: (context, state) {
+      return Scaffold(
+          body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Colors.black87,
+                    Colors.blue,
+                    Colors.white,
+                  ],
+                  stops: [
+                    0.01,
+                    0.5,
+                    0.9,
+                  ],
+                ),
+              ),
+              child: Center(
+                child: Container(
+                  constraints:
+                      const BoxConstraints(maxWidth: 800, minHeight: 550),
+                  width: mediaSize.width,
+                  height: mediaSize.height,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Logo(),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        FormLogin(
+                          login: (value) {
+                            setState(() {
+                              login = value;
+                            });
+                          },
+                          password: (value) {
+                            setState(() {
+                              password = value;
+                            });
+                          },
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            ButtonLogin(
+                              functionLogin: () {
+                                _controller.validateTokenWithLoginUsecase(
+                                  login: login,
+                                  password: password,
+                                );
+                              },
+                              animationErro: animationErro,
+                              login: login,
+                              password: password,
+                            ),
+                            const SizedBox(
+                              height: 50,
+                            ),
+                            const ButtonForgotPassword(),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        const ButtonCreateAccount()
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (_, __) {
+                    return Container(
+                      width: 0,
+                      height: 0,
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.black87,
+                          Colors.blue,
+                          Colors.white,
+                        ],
+                        stops: [
+                          0.01,
+                          0.5,
+                          0.9,
+                        ],
+                      )),
+                    );
+                  }),
+            )
+          ],
+        ),
+      ));
+    });
+  }
+}
