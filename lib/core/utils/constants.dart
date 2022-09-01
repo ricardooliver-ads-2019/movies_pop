623e04ead203339c2b0f3b08839cdc67993a793b
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 09:13:25 -0400

    refactor: Melhorando os nomes dos atributos usando o padrão lowerCamelCase.

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,9 +1,14 @@
 class Constants {
   Constants._();
-  static const base_url = 'https://api.themoviedb.org/3';
-
-  static const api_key = '899e6212bb39852b379032c9c5e25689';
+  static const baseUrl = 'https://api.themoviedb.org/3';
+  static const apiKey = '899e6212bb39852b379032c9c5e25689';
+  static const messageAddMoveiSuccess = 'Filme adicionando com sucesso!';
+  static const messageRemoveMoveiSuccess = 'Filme removido com sucesso!';
+  static const messageAddMoveiError = 'Erro ao adicionar filme!';
+  static const messageRemoveMoveiError = 'Erro ao remover filme!';
   static const sharedPreferencesUsername = 'userName';
   static const sharedPreferencesAccountId = 'accountId';
   static const sharedPreferencesSessionId = 'sessionId';
+  static const sharedPreferencesNameListWatchMovies = 'nameListWatchMovies';
+  static const sharedPreferencesIdListWatchMovies = 'idListWatchMovies';
 }
