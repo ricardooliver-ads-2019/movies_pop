8a04f2091e5c3c916d79eb9d3d819104e00d62bd
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 09:28:09 -0400

    refactor: Atualizando os imports

--- lib/features/movies/home/domain/usecase/get_movies_playing_in_Brazil_now_usecase.dart
+++ lib/features/movies/home/domain/usecase/get_movies_playing_in_Brazil_now_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_pop/core/erros/failures.dart';
-import 'package:movies_pop/features/movies/home/domain/entities/movies_page_entipy/movies_page_entipy.dart';
+import 'package:movies_pop/features/shared/entities/movies_page_entipy/movies_page_entipy.dart';
 
 import '../repositories/home/home_repository.dart';
 
--- lib/features/movies/home/domain/usecase/get_movies_popular_usecar.dart
+++ lib/features/movies/home/domain/usecase/get_movies_popular_usecar.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_pop/core/erros/failures.dart';
-import 'package:movies_pop/features/movies/home/domain/entities/movies_page_entipy/movies_page_entipy.dart';
 import 'package:movies_pop/features/movies/home/domain/repositories/home/home_repository.dart';
+import 'package:movies_pop/features/shared/entities/movies_page_entipy/movies_page_entipy.dart';
 
 class GetMoviesPopularUsecar {
   final HomeRepository _repository;
