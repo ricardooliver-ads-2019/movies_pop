afd0b59bf3eb053678c1640a25f27b89bb0ee052
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 10:24:34 -0400

    feat: Criando a classe ListWatchaedMoviesIdEntity, que será a interface da regra de negócio relacionado ao padrão da lista de filmes já assistidos pelo usuario.

---
+++ lib/features/movies/home/domain/entities/lists_movies__entities/list_watchaed_movies_Id_entity.dart
@@ -0,0 +1,9 @@
+import 'package:equatable/equatable.dart';
+
+class ListWatchaedMoviesIdEntity extends Equatable {
+  final int idListWatchedMovies;
+  const ListWatchaedMoviesIdEntity({required this.idListWatchedMovies});
+
+  @override
+  List<Object?> get props => [];
+}
