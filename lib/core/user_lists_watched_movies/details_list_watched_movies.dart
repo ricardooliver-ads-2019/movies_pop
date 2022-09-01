b90509f904048c77d5692c1dea07ce8e39441d8a
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 08:53:49 -0400

    feat: Adicionando classe responsavel por gerenciar o id da lista de filmes já assistidos do usuario     - Criando a classe que gerencia os dados da lista de filmes     já assistidos do usuario  que ajuda a aplicação a identificar a lista que e fazer a busca dos filmes.

---
+++ lib/core/user_lists_watched_movies/details_list_watched_movies.dart
@@ -0,0 +1,28 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../utils/constants.dart';
+
+class DetailsListWatchedMovies {
+  int? idList;
+
+  Future<SharedPreferences> getInstancySharedPreferences() async {
+    return await SharedPreferences.getInstance();
+  }
+
+  Future<void> init() async {
+    final preferences = await getInstancySharedPreferences();
+    idList = preferences.getInt(Constants.sharedPreferencesIdListWatchMovies);
+    print(idList);
+  }
+
+  Future<void> saveIdListWatchMovies({required int idList}) async {
+    final preferences = await getInstancySharedPreferences();
+    await preferences.setInt(
+        Constants.sharedPreferencesIdListWatchMovies, idList);
+  }
+
+  Future<void> resetIdListWatchMovies({required String sessionId}) async {
+    final preferences = await getInstancySharedPreferences();
+    await preferences.remove(Constants.sharedPreferencesIdListWatchMovies);
+  }
+}
