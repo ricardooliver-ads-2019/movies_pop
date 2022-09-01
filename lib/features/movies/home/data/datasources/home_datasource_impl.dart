807c163d9fe41e62e876ce1045ba6ace10256a5c
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 11:31:37 -0400

    feat: Adicionando os novos endpoits a camada data(datasources) responsaveis por fazer a requisição de buscar as listas de filmes criadas pelo usuario e a requisição de criar uma lista de filmes já assistidos caso o usuario não possua.

--- lib/features/movies/home/data/datasources/home_datasource_impl.dart
+++ lib/features/movies/home/data/datasources/home_datasource_impl.dart
@@ -1,31 +1,61 @@
+import 'package:movies_pop/core/auth/auth.dart';
 import 'package:movies_pop/core/network/http_client.dart';
 import 'package:movies_pop/core/network/http_client_response.dart';
+import 'package:movies_pop/core/utils/constants.dart';
 import 'package:movies_pop/features/movies/home/data/datasources/i_home_datasource.dart';
 
 class HomeDatasourceImpl implements IHomeDatasource {
   final HttpClient _client;
-  HomeDatasourceImpl({required HttpClient client}) : _client = client;
+  final AuthSession _auth;
+  HomeDatasourceImpl({required HttpClient client, required AuthSession auth})
+      : _client = client,
+        _auth = auth;
 
   @override
   Future<HttpClientResponse> getMoviesPlayingInBrazilNow(
       {required int page}) async {
-    return await _client.get('https://api.themoviedb.org/3/movie/now_playing',
-        queryParameters: {
-          'api_key': '899e6212bb39852b379032c9c5e25689',
-          'language': 'pt-br',
-          'regio': 'Brazil',
-          'page': page
-        });
+    return await _client.get(
+      Constants.baseUrl + '/movie/now_playing',
+      queryParameters: {
+        'api_key': Constants.apiKey,
+        'language': 'pt-br',
+        'regio': 'Brazil',
+        'page': page
+      },
+    );
   }
 
   @override
   Future<HttpClientResponse> getMoviesPopular({required int page}) async {
     return await _client
-        .get('https://api.themoviedb.org/3/movie/popular', queryParameters: {
-      'api_key': '899e6212bb39852b379032c9c5e25689',
+        .get(Constants.baseUrl + '/movie/popular', queryParameters: {
+      'api_key': Constants.apiKey,
       'language': 'pt-br',
       'regio': 'Brazil',
       'page': page
     });
+  }
+
+  @override
+  Future<HttpClientResponse> createMyListWatchedMovies() async {
+    return await _client.post(Constants.baseUrl + '/list', queryParameters: {
+      'api_key': Constants.apiKey,
+      'session_id': _auth.sessionId,
+    }, data: {
+      'name': 'filmes_já_vistos',
+      'description': 'lista de filmes que já assisti',
+      'language': 'pt'
+    });
+  }
+
+  @override
+  Future<HttpClientResponse> getMylists() async {
+    return await _client.get(
+      Constants.baseUrl + '/account/${_auth.id}/lists',
+      queryParameters: {
+        'api_key': Constants.apiKey,
+        'session_id': _auth.sessionId,
+      },
+    );
   }
 }
--- lib/features/movies/home/data/datasources/i_home_datasource.dart
+++ lib/features/movies/home/data/datasources/i_home_datasource.dart
@@ -3,4 +3,6 @@ import 'package:movies_pop/core/network/http_client_response.dart';
 abstract class IHomeDatasource {
   Future<HttpClientResponse> getMoviesPlayingInBrazilNow({required int page});
   Future<HttpClientResponse> getMoviesPopular({required int page});
+  Future<HttpClientResponse> getMylists();
+  Future<HttpClientResponse> createMyListWatchedMovies();
 }
