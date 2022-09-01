8359eaec607d6035bb85810f53ff5e0cf5f86f9a
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 08:18:35 -0400

    feat: Adicionando dependencies para injeção

--- lib/core/dependencies/get_it/dependencies.dart
+++ lib/core/dependencies/get_it/dependencies.dart
@@ -1,9 +1,20 @@
 import 'package:get_it/get_it.dart';
+import 'package:movies_pop/core/auth/auth.dart';
 import 'package:movies_pop/core/network/dio/dio_client.dart';
 import 'package:movies_pop/core/network/http_client.dart';
 import 'package:movies_pop/core/network_connection/i_network_connection.dart';
 import 'package:movies_pop/core/network_connection/network_connection_impl.dart';
-import 'package:movies_pop/features/auth/auth.dart';
+import 'package:movies_pop/core/user_lists_watched_movies/details_list_watched_movies.dart';
+import 'package:movies_pop/features/fab_button_menu/data/datasources/get_status_movies_datasources.dart';
+import 'package:movies_pop/features/fab_button_menu/data/datasources/get_status_movies_datasources_impl.dart';
+import 'package:movies_pop/features/fab_button_menu/data/repositories/fab_button_menu_repository_impl.dart';
+import 'package:movies_pop/features/fab_button_menu/domain/repositories/fab_button_menu_repository.dart';
+import 'package:movies_pop/features/fab_button_menu/domain/usecase/add_movie_to_watch_movies_list_usecase.dart';
+import 'package:movies_pop/features/fab_button_menu/domain/usecase/add_movie_to_watched_movies_list_usecase.dart';
+import 'package:movies_pop/features/fab_button_menu/domain/usecase/check_movie_in_my_list_watched_movies_usecase.dart';
+import 'package:movies_pop/features/fab_button_menu/domain/usecase/get_status_movies_usecase.dart';
+import 'package:movies_pop/features/fab_button_menu/domain/usecase/remove_movie_to_watched_movies_list_usecase.dart';
+import 'package:movies_pop/features/fab_button_menu/presenter/controller/fab_button_cubit_controller.dart';
 import 'package:movies_pop/features/login/data/datasources/i_login_datasources.dart';
 import 'package:movies_pop/features/login/data/datasources/login_datasources_impl.dart';
 import 'package:movies_pop/features/login/data/repositories/login_repository_impl.dart';
@@ -21,8 +32,25 @@ import 'package:movies_pop/features/movies/home/data/repositories/home_repositor
 import 'package:movies_pop/features/movies/home/domain/repositories/home/home_repository.dart';
 import 'package:movies_pop/features/movies/home/domain/usecase/get_movies_playing_in_Brazil_now_usecase.dart';
 import 'package:movies_pop/features/movies/home/domain/usecase/get_movies_popular_usecar.dart';
+import 'package:movies_pop/features/movies/home/domain/usecase/usecase_list_user/create_my_list_watched_movies_usecase.dart';
+import 'package:movies_pop/features/movies/home/domain/usecase/usecase_list_user/get_my_lists_usecase.dart';
 import 'package:movies_pop/features/movies/home/presenter/components/cine_movies/cine_movies_cubit_controller/cine_movies_cubit_controller.dart';
 import 'package:movies_pop/features/movies/home/presenter/components/popular_movies/popular_cubit_controller/popular_cubit_controller.dart';
+import 'package:movies_pop/features/movies/home/presenter/screens/controller/home_cubit_controller.dart';
+import 'package:movies_pop/features/watch_movies/data/datasources/watch_movies_datasources.dart';
+import 'package:movies_pop/features/watch_movies/data/datasources/watch_movies_datasources_impl.dart';
+import 'package:movies_pop/features/watch_movies/data/repositories/watch_movies_repository_impl.dart';
+import 'package:movies_pop/features/watch_movies/domain/repositories/watch_movies_repository.dart';
+import 'package:movies_pop/features/watch_movies/domain/usecase/get_list_watch_movies_usecase.dart';
+import 'package:movies_pop/features/watch_movies/presenter/controller/watch_movies_cubit_controller.dart';
+import 'package:movies_pop/features/watched_movies/data/datasources/watchaed_movies_datasource.dart';
+import 'package:movies_pop/features/watched_movies/data/datasources/watchaed_movies_datasource_impl.dart';
+import 'package:movies_pop/features/watched_movies/data/repositories/watchaed_movies_repository_impl.dart';
+import 'package:movies_pop/features/watched_movies/domain/repositories/watchaed_movies_repository.dart';
+import 'package:movies_pop/features/watched_movies/domain/usecase/get_my_list_watched_movies_usecase.dart';
+import 'package:movies_pop/features/watched_movies/presenter/controller/watched_cubit_controller.dart';
+
+import '../../../features/fab_button_menu/domain/usecase/remove_movie_to_watch_movies_list_usecase.dart';
 
 GetIt getItDependency = GetIt.instance;
 
@@ -32,9 +60,64 @@ Future<void> getItDependencies() async {
       .registerFactory<INetworkConnection>(() => NetworkConnectionImpl());
 
   getItDependency.registerSingleton<AuthSession>(AuthSession()..init());
+  getItDependency.registerSingleton<DetailsListWatchedMovies>(
+      DetailsListWatchedMovies()..init());
 
   getItDependency.registerLazySingleton<HttpClient>(
       () => DioClient(networkConnection: getItDependency()));
+
+//=========================== FabButton =======================
+
+  getItDependency.registerFactory<FabButtonMenuDatasources>(() =>
+      FabButtonMenuDatasourcesImpl(
+        detailsListWatchedMovies: getItDependency<DetailsListWatchedMovies>(),
+        auth: getItDependency<AuthSession>(),
+        client: getItDependency<HttpClient>(),
+      ));
+
+  getItDependency.registerFactory<FabButtonMenuRepository>(() =>
+      FabButtonMenuRepositoryImpl(
+          fabButtonMenuDatasources:
+              getItDependency<FabButtonMenuDatasources>()));
+
+  getItDependency.registerFactory<GetStatusMoviesUsecase>(() =>
+      GetStatusMoviesUsecase(
+          fabButtonMenuRepository: getItDependency<FabButtonMenuRepository>()));
+
+  getItDependency.registerFactory<CheckMovieInMyListWatchedMoviesUsecase>(() =>
+      CheckMovieInMyListWatchedMoviesUsecase(
+          fabButtonMenuRepository: getItDependency<FabButtonMenuRepository>()));
+
+  getItDependency.registerFactory<AddMovieToWatchedMoviesListUsecase>(() =>
+      AddMovieToWatchedMoviesListUsecase(
+          fabButtonMenuRepository: getItDependency<FabButtonMenuRepository>()));
+
+  getItDependency.registerFactory<RemoveMovieToWatchedMoviesListUsecase>(() =>
+      RemoveMovieToWatchedMoviesListUsecase(
+          fabButtonMenuRepository: getItDependency<FabButtonMenuRepository>()));
+
+  getItDependency.registerFactory<AddMovieToWatchMoviesListUsecase>(() =>
+      AddMovieToWatchMoviesListUsecase(
+          fabButtonMenuRepository: getItDependency<FabButtonMenuRepository>()));
+
+  getItDependency.registerFactory<RemoveMovieToWatchMoviesListUsecase>(() =>
+      RemoveMovieToWatchMoviesListUsecase(
+          fabButtonMenuRepository: getItDependency<FabButtonMenuRepository>()));
+
+  getItDependency
+      .registerFactory<FabButtonCubitController>(() => FabButtonCubitController(
+            checkMovieInMyListWatchedMoviesUsecase:
+                getItDependency<CheckMovieInMyListWatchedMoviesUsecase>(),
+            getStatusMoviesUsecase: getItDependency<GetStatusMoviesUsecase>(),
+            addMovieToWatchedMoviesListUsecase:
+                getItDependency<AddMovieToWatchedMoviesListUsecase>(),
+            removeMovieToWatchedMoviesListUsecase:
+                getItDependency<RemoveMovieToWatchedMoviesListUsecase>(),
+            addMovieToWatchMoviesListUsecase:
+                getItDependency<AddMovieToWatchMoviesListUsecase>(),
+            removeMovieToWatchMoviesListUsecase:
+                getItDependency<RemoveMovieToWatchMoviesListUsecase>(),
+          ));
 
 //=========================== Login ===========================
 
@@ -55,10 +138,23 @@ Future<void> getItDependencies() async {
       GetDetailsAccountUsecase(
           loginRepository: getItDependency<ILoginRepository>()));
 
+  getItDependency.registerFactory<LoginCubitController>(
+    () => LoginCubitController(
+      authSession: getItDependency<AuthSession>(),
+      getRequestTokenusecase: getItDependency<GetRequestTokenUsecase>(),
+      validateTokenWithLoginUsecase:
+          getItDependency<ValidateTokenWithLoginUsecase>(),
+      validateSessionIdUsecase: getItDependency<ValidateSessionIdUsecase>(),
+      getDetailsAccountUsecase: getItDependency<GetDetailsAccountUsecase>(),
+      detailsListWatchedMovies: getItDependency<DetailsListWatchedMovies>(),
+    ),
+  );
+
 //=========================== Home ===========================
 
-  getItDependency.registerFactory<IHomeDatasource>(
-      () => HomeDatasourceImpl(client: getItDependency<HttpClient>()));
+  getItDependency.registerFactory<IHomeDatasource>(() => HomeDatasourceImpl(
+      client: getItDependency<HttpClient>(),
+      auth: getItDependency<AuthSession>()));
 
   getItDependency.registerFactory<HomeRepository>(
       () => HomeRepositoryImpl(datasource: getItDependency<IHomeDatasource>()));
@@ -70,16 +166,63 @@ Future<void> getItDependencies() async {
   getItDependency.registerFactory<GetMoviesPopularUsecar>(() =>
       GetMoviesPopularUsecar(repository: getItDependency<HomeRepository>()));
 
+  getItDependency.registerFactory<GetMyListsUsecase>(() =>
+      GetMyListsUsecase(homeRepository: getItDependency<HomeRepository>()));
+
+  getItDependency.registerFactory<CreateMyListWatchedMoviesUsecase>(() =>
+      CreateMyListWatchedMoviesUsecase(
+          homeRepository: getItDependency<HomeRepository>()));
+
+  getItDependency.registerFactory<HomeCubitController>(() =>
+      HomeCubitController(
+          getMyListsUsecase: getItDependency<GetMyListsUsecase>(),
+          createMyListWatchedMoviesUsecase:
+              getItDependency<CreateMyListWatchedMoviesUsecase>(),
+          detailsListWatchedMovies:
+              getItDependency<DetailsListWatchedMovies>()));
+
+//=========================== WatchMovies ===========================
+
+  getItDependency.registerFactory<WatchMoviesDatasources>(() =>
+      WatchMoviesDatasourcesImpl(
+          client: getItDependency<HttpClient>(),
+          auth: getItDependency<AuthSession>()));
+
+  getItDependency.registerFactory<WatchMoviesRepository>(() =>
+      WatchMoviesRepositoryImpl(
+          watchMoviesDatasources: getItDependency<WatchMoviesDatasources>()));
+
+  getItDependency.registerFactory<WatchMoviesCubitController>(() =>
+      WatchMoviesCubitController(
+          usecase: getItDependency<GetListWatchMoviesUsecase>()));
+
+  getItDependency.registerFactory<GetListWatchMoviesUsecase>(() =>
+      GetListWatchMoviesUsecase(
+          watchMoviesRopository: getItDependency<WatchMoviesRepository>()));
+
+//=========================== WatchedMovies ===========================
+
+  getItDependency.registerFactory<WatchaedMoviesDatasource>(() =>
+      WatchaedMoviesDatasourceImpl(
+          client: getItDependency<HttpClient>(),
+          idList: getItDependency<DetailsListWatchedMovies>()));
+
+  getItDependency.registerFactory<WatchaedMoviesRepository>(() =>
+      WatchaedMoviesRepositoryImpl(
+          datasource: getItDependency<WatchaedMoviesDatasource>()));
+
+  getItDependency.registerFactory<GetMyListWatchedMoviesUsecase>(() =>
+      GetMyListWatchedMoviesUsecase(
+          repository: getItDependency<WatchaedMoviesRepository>()));
+
+  getItDependency
+      .registerFactory<WatchedCubitController>(() => WatchedCubitController(
+            detailsListWatchedMovies:
+                getItDependency<DetailsListWatchedMovies>(),
+            getMyListWatchedMoviesUsecase:
+                getItDependency<GetMyListWatchedMoviesUsecase>(),
+          ));
 //=========================== Cubits ===========================
-  getItDependency.registerFactory<LoginCubitController>(() =>
-      LoginCubitController(
-        authSession: getItDependency<AuthSession>(),
-        getRequestTokenusecase: getItDependency<GetRequestTokenUsecase>(),
-        validateTokenWithLoginUsecase:
-            getItDependency<ValidateTokenWithLoginUsecase>(),
-        validateSessionIdUsecase: getItDependency<ValidateSessionIdUsecase>(),
-        getDetailsAccountUsecase: getItDependency<GetDetailsAccountUsecase>(),
-      ));
 
   getItDependency.registerFactory<MainNavigationPageCubit>(
       () => MainNavigationPageCubit());
