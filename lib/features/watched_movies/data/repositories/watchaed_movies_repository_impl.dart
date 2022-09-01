bbfed5c6092061bc1a9cf18160a3b3d188ac8ed7
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 18:00:35 -0400

    feat: Adicionando a camada de repositories, camada responsavel por gerenciar e tratar as requisições vinda do usecase e destinar para a camada do datasources, como também receber a resposta vinda do datasources e fazer o tratamento de dados para a converção em um modelo que a aplicação conheça e possa apresentar para os usuários.

---
+++ lib/features/watched_movies/data/repositories/watchaed_movies_repository_impl.dart
@@ -0,0 +1,54 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_pop/core/erros/failures.dart';
+import 'package:movies_pop/core/network/http_client_response.dart';
+import 'package:movies_pop/features/watched_movies/data/models/list_watched_movies_model.dart';
+import 'package:movies_pop/features/watched_movies/domain/entities/list_watched_movies_entity.dart';
+import 'package:movies_pop/features/watched_movies/domain/repositories/watchaed_movies_repository.dart';
+
+import '../datasources/watchaed_movies_datasource.dart';
+
+class WatchaedMoviesRepositoryImpl implements WatchaedMoviesRepository {
+  final WatchaedMoviesDatasource _datasource;
+  WatchaedMoviesRepositoryImpl({required WatchaedMoviesDatasource datasource})
+      : _datasource = datasource;
+
+  @override
+  Future<Either<Failure, ListWatchedMoviesEntity>> getListWatchedMovies(
+      {required int idList}) async {
+    final result = await _datasource.getListWatchedMovies();
+    if (result is HttpClientResponseError) {
+      if (result.statusCode == 0) {
+        return Left(GenericFailure(
+          error: result.data,
+          message: result.statusMessage,
+          statusCode: result.statusCode,
+        ));
+      }
+
+      return Left(GenericFailure(
+        error: result.data,
+        message: result.statusMessage,
+        statusCode: result.statusCode,
+      ));
+    }
+
+    // if ((result.data == null) || (result.data['results'] as List).isEmpty) {
+    //   return const Left(GenericFailure(
+    //     error: 'Lista de filmes vazia',
+    //     message: 'Erro ao buscar lista de filmes',
+    //     statusCode: 000,
+    //   ));
+    // }
+
+    try {
+      final results = ListWatchedMoviesModel.fromJson(result.data);
+      return Right(results);
+    } catch (e) {
+      return const Left(GenericFailure(
+        message: 'Erro de conversão',
+        error: 'xxListWatchedMoviesxx',
+        statusCode: 500,
+      ));
+    }
+  }
+}
