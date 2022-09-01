b7979c8cc5495b30d1b6161bca18b0b19a792c42
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-01 12:09:49 -0400

    feat: Adicionando ao repositories da cama data da home as funcionalidades na buscar as lista do usuário e a funcionalidade de criar uma lista de filmes para Adicionandofilmes que ele já assistiu.

--- lib/features/movies/home/data/repositories/home_repository_impl.dart
+++ lib/features/movies/home/data/repositories/home_repository_impl.dart
@@ -1,10 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_pop/core/erros/failures.dart';
 import 'package:movies_pop/core/network/http_client_response.dart';
-import 'package:movies_pop/features/movies/home/data/models/movie_model.dart';
-import 'package:movies_pop/features/movies/home/data/models/movies_page_model.dart';
-import 'package:movies_pop/features/movies/home/domain/entities/movies_page_entipy/movies_page_entipy.dart';
+import 'package:movies_pop/features/movies/home/data/models/list_watchaed_movies_Id_model.dart';
+import 'package:movies_pop/features/movies/home/data/models/my_lists_movies_model.dart';
+import 'package:movies_pop/features/movies/home/domain/entities/lists_movies__entities/list_watchaed_movies_Id_entity.dart';
+import 'package:movies_pop/features/movies/home/domain/entities/lists_movies__entities/my_lists_movies_entity.dart';
 import 'package:movies_pop/features/movies/home/domain/repositories/home/home_repository.dart';
+import 'package:movies_pop/features/shared/entities/movies_page_entipy/movies_page_entipy.dart';
+import 'package:movies_pop/features/shared/models/movie_model/movie_model.dart';
+import 'package:movies_pop/features/shared/models/movies_page_model/movies_page_model.dart';
 
 import '../datasources/i_home_datasource.dart';
 
@@ -98,6 +102,67 @@ class HomeRepositoryImpl implements HomeRepository {
       return const Left(GenericFailure(
           message: 'Erro de conversão',
           error: 'xxMoviePagexx',
+          statusCode: 500));
+    }
+  }
+
+  @override
+  Future<Either<Failure, ListWatchaedMoviesIdEntity>>
+      createMyListWatchedMovies() async {
+    final result = await _datasource.createMyListWatchedMovies();
+    if (result is HttpClientResponseError) {
+      if (result.statusCode == 0) {
+        return Left(GenericFailure(
+          error: result.data,
+          message: result.statusMessage,
+          statusCode: result.statusCode,
+        ));
+      }
+
+      return Left(GenericFailure(
+        error: result.data,
+        message: result.statusMessage,
+        statusCode: result.statusCode,
+      ));
+    }
+
+    try {
+      final results = ListWatchaedMoviesIdModel.fromJson(result.data);
+      return Right(results);
+    } catch (e) {
+      return const Left(GenericFailure(
+          message: 'Erro de conversão',
+          error: 'xxMyListsMoviesxx',
+          statusCode: 500));
+    }
+  }
+
+  @override
+  Future<Either<Failure, MyListsMoviesEntity>> getMyLists() async {
+    final result = await _datasource.getMylists();
+    if (result is HttpClientResponseError) {
+      if (result.statusCode == 0) {
+        return Left(GenericFailure(
+          error: result.data,
+          message: result.statusMessage,
+          statusCode: result.statusCode,
+        ));
+      }
+
+      return Left(GenericFailure(
+        error: result.data,
+        message: result.statusMessage,
+        statusCode: result.statusCode,
+      ));
+    }
+
+    try {
+      final results = MyListsMoviesModel.fromJson(result.data);
+      return Right(results);
+    } catch (e) {
+      return const Left(GenericFailure(
+          message: 'Erro de conversão',
+          error: 'xxMyListsMoviesxx',
           statusCode: 500));
     }
   }
