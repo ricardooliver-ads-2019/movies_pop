7686aa799d830713bcafb226abe483d03cd86154
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2022-09-14 13:59:03 -0400

    feat: Adicionando class para ajudar na organição e padronização do thema do app.

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Colors.white;
+  static const secondary = Colors.blue;
+  static const grey = Color(0xFF585666);
+  static const delete = Color.fromARGB(255, 216, 119, 119);
+  static const heading = Color(0xFF585666);
+  static const check = Color.fromARGB(255, 137, 199, 118);
+  static const stroke = Color(0xFFE3E3E6);
+  static const shape = Color(0xFFFAFAFC);
+  static const background = Color(0xFFFFFFFF);
+  static const input = Color(0xFFB1B0B8);
+  static const stars = Color.fromARGB(255, 249, 168, 37);
+}
---
+++ lib/core/theme/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static const logoFull = "assets/images/";
+  static const logomini = "assets/images/";
+  static const union = "assets/images/";
+  static const person = "assets/images/";
+  static const google = "assets/images/";
+}
